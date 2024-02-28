918423f009274f027c3c99854eeec535cde7eddd
Sarah <sarahchae0219@gmail.com>
2024-02-28 14:53:19 -0500

    frontend update

---
+++ client/src/service/tweet.js
@@ -0,0 +1,43 @@
+export default class TweetService {
+  tweets = [
+    {
+      id: 1,
+      text: 'Hello World',
+      createdAt: '2024-02-28T04:20:57.000Z',
+      name: 'Bob',
+      username: 'bob',
+      url: 'https://www.pexels.com/photo/men-s-blue-dress-shirt-1043471/',
+    },
+  ];
+
+  async getTweets(username) {
+    return username
+      ? this.tweets.filter((tweet) => tweet.username === username)
+      : this.tweets;
+  }
+
+  async postTweet(text) {
+    const tweet = {
+      id: Date.now(),
+      createdAt: new Date(),
+      name: 'Ellie',
+      username: 'ellie',
+      text,
+    };
+    this.tweets.push(tweet);
+    return tweet;
+  }
+
+  async deleteTweet(tweetId) {
+    this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
+  }
+
+  async updateTweet(tweetId, text) {
+    const tweet = this.tweets.find((tweet) => tweet.id === tweetId);
+    if (!tweet) {
+      throw new Error('tweet not found!');
+    }
+    tweet.text = text;
+    return tweet;
+  }
+}
