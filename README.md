4cd59f3c31a7cfa8abaff8bdf45bfb9320d62d86
Sarah <sarahchae0219@gmail.com>
2024-03-06 13:26:17 -0600

    update

---
+++ README.md
@@ -0,0 +1,80 @@
+# dwitter API Doc 
+
+## Tweets
+
+**Tweet Schema**
+```
+JSON
+{
+    id: string, // Twitter Id
+    text: string, // Twitter content
+    createdAt: Date, // Date of Twitter created
+    name: string, // Name of user
+    username: string, // Username of user
+    url: string (optional) // user profile's url
+}
+```
+**GET/tweets**
+```diff
+get all tweets
+
+- Response 200
+{
+    [tweet, tweet, ...]
+}
+```
+**GET/tweets?username=:username**
+```diff
+get all tweets for user's username
+
+- Response 200
+{
+    [tweet, tweet, ...]
+}
+```
+**GET/tweets/:id**
+```diff
+get tweet by id
+
+- Response 200
+{
+    tweet
+}
+```
+**POST/tweets**
+```diff
+create new tweet
+
+Request
+{
+    text,
+    name,
+    username,
+    url (optional)
+}
+
+- Response 201
+{
+    tweet
+}
+```
+**PUT/tweets/:id**
+```diff
+update tweet
+
+Request
+{
+    text
+}
+
+- Response 200
+{
+    tweet
+}
+```
+**DELETE/tweets/:id**
+```diff
+delete tweet
+
+- Response 204
+```
---
+++ server/data/tweet.js
@@ -0,0 +1,53 @@
+let tweets = [
+    {
+        id: "1",
+        text: "Hello world",
+        createdAt: Date.now().toString(),
+        name: 'Bob',
+        username: 'bob',
+        //url: 'https://www.pexels.com/photo/men-s-blue-dress-shirt-1043471/',
+    }, 
+    {
+        id: "2",
+        text: "Hi :)",
+        createdAt: Date.now().toString(),
+        name: 'Sarah',
+        username: 'sarah',
+    }
+];
+
+export function getALL() {
+    return tweets;
+}
+
+export function getByUsername(username) {
+    return tweets.filter((tweet) => tweet.username === username);
+}
+
+export function getById(id) {
+    return tweets.find((tweet) => tweet.id === id);
+}
+
+export function create(text, name, usernmae) {
+    const tweet = {
+        id: Date.now().toString(),
+        text,
+        createdAt: new Date(),
+        name,
+        username,
+    };
+    tweets = [tweet, ...tweets];
+    return tweets;
+}
+
+export function update(id, text) {
+    const tweet = tweets.find((tweet) => tweet.id === id);
+    if (tweet) {
+        tweet.text = text;
+    }
+    return tweet;
+}
+
+export function remove(id) {
+    tweets =  tweets.filter((tweet) => tweet.id !== id);
+}
