4867f39c7fd066b5b8e5969015d921f8138602b7
Sarah <sarahchae0219@gmail.com>
2024-03-13 15:26:22 -0500

    authentication implemented

--- server/data/tweet.js
+++ server/data/tweet.js
@@ -1,17 +1,17 @@
-import * as userRepository from './auth.js';
+import * as userRepository from "./auth.js";
 
 let tweets = [
   {
-    id: '1',
-    text: '드림코더분들 화이팅!',
+    id: "1",
+    text: "hey",
     createdAt: new Date().toString(),
-    userId: '1',
+    userId: "1",
   },
   {
-    id: '2',
-    text: '안뇽!',
+    id: "2",
+    text: "Hello World",
     createdAt: new Date().toString(),
-    userId: '1',
+    userId: "1",
   },
 ];
 
