1be4528cf95fb2302cdb4f611f9fb41d6be906c0
Sarah <sarahchae0219@gmail.com>
2024-03-13 14:50:18 -0500

    updated

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,11 +1,11 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import {} from "express-async-errors";
-import * as userRepository from "../data/auth.js";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import {} from 'express-async-errors';
+import * as userRepository from '../data/auth.js';
 
 // TODO: Make it secure!
-const jwtSecretKey = "F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z";
-const jwtExpiresInDays = "2d";
+const jwtSecretKey = 'F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z';
+const jwtExpiresInDays = '2d';
 const bcryptSaltRounds = 12;
 
 export async function signup(req, res) {
@@ -30,11 +30,11 @@ export async function login(req, res) {
   const { username, password } = req.body;
   const user = await userRepository.findByUsername(username);
   if (!user) {
-    return res.status(401).json({ message: "Invalid user or password" });
+    return res.status(401).json({ message: 'Invalid user or password' });
   }
   const isValidPassword = await bcrypt.compare(password, user.password);
   if (!isValidPassword) {
-    return res.status(401).json({ message: "Invalid user or password" });
+    return res.status(401).json({ message: 'Invalid user or password' });
   }
   const token = createJwtToken(user.id);
   res.status(200).json({ token, username });
@@ -47,7 +47,7 @@ function createJwtToken(id) {
 export async function me(req, res, next) {
   const user = await userRepository.findById(req.userId);
   if (!user) {
-    return res.status(404).json({ message: "User not found" });
+    return res.status(404).json({ message: 'User not found' });
   }
   res.status(200).json({ token: req.token, username: user.username });
 }
--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,35 +1,34 @@
-import express from "express";
-import "express-async-errors";
-import { body } from "express-validator";
-import * as tweetController from "../controller/tweet.js";
-import { validate } from "../middleware/validator.js";
-import { isAuth } from "../middleware/auth.js";
+import express from 'express';
+import 'express-async-errors';
+import { body } from 'express-validator';
+import * as tweetController from '../controller/tweet.js';
+import { isAuth } from '../middleware/auth.js';
+import { validate } from '../middleware/validator.js';
+
 const router = express.Router();
 
-// validation
-// sanitization & normalization
 const validateTweet = [
-  body("text")
+  body('text')
     .trim()
     .isLength({ min: 3 })
-    .withMessage("text should be at least 3 characters"),
+    .withMessage('text should be at least 3 characters'),
   validate,
 ];
 
-// GET /tweets
+// GET /tweet
 // GET /tweets?username=:username
-router.get("/", isAuth, tweetController.getTweets);
+router.get('/', isAuth, tweetController.getTweets);
 
 // GET /tweets/:id
-router.get("/:id", isAuth, tweetController.getTweet);
+router.get('/:id', isAuth, tweetController.getTweet);
 
 // POST /tweeets
-router.post("/", isAuth, validateTweet, tweetController.createTweet);
+router.post('/', isAuth, validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put("/:id", isAuth, validateTweet, tweetController.updateTweet);
+router.put('/:id', isAuth, validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets/:id
-router.delete("/:id", isAuth, tweetController.deleteTweet);
+router.delete('/:id', isAuth, tweetController.deleteTweet);
 
 export default router;
